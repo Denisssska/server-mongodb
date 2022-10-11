2f6e0807da67c74c5f03e5cf1c7b2809f22d7121
Denisssska <yarmoshkoden18m@gmail.com>
2022-10-11 16:38:30 +0300

    добавил условие для папки uploads

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 let allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
