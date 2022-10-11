2149fcd0896f7bfe806645ecb501b7c1ce7e29d9
Denisssska <yarmoshkoden18m@gmail.com>
2022-10-11 16:37:42 +0300

    добавил условие для папки uploads

--- index.js
+++ index.js
@@ -31,14 +31,24 @@ mongoose.connect(process.env.MONGODB_URI)
         }
     ).catch((err) => console.log('db error', err));
 
-const corsOptions ={
-    origin:'*',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-}
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
 
 const app = express()
-app.use(cors(corsOptions))
+
+app.use(cors())
+app.use(allowCrossDomain)
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))//чтобы можно было открыть картинку в урле
 
