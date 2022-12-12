d8c909d74216e0259ab36ba31643e8d87dedf1b4
Denisssska <yarmoshkoden18m@gmail.com>
2022-12-12 23:15:02 +0300

    cors refactor

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.MONGODB_URI || MongoDBDen)
     ).catch((err) => console.log('db error', err));
 
 let allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://front-posts-cxx9vtncr-denisssska.vercel.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
     // intercept OPTIONS method
