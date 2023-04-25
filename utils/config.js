3134ad95ba589e5b1b174dbcd55ba7263a503a8a
Denisssska <yarmoshkoden18m@gmail.com>
2023-04-25 18:05:44 +0300

    add .env

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
-// const USER_NAME = process.env.MONGO_DB_USER_NAME;
-// const PASSWORD = process.env.MONGO_DB_USER_PASSWORD;
-// const MONGO_DB_URL = process.env.MONGO_DB_URL; // bd for tests
+const USER_NAME = process.env.MONGO_DB_USER_NAME;
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD;
+const MONGO_DB_URL = process.env.MONGO_DB_URL; // bd for tests
 export const MongoDBDen =process.env.MONGO_DB_URI
     // `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority&tls=true`;
 
